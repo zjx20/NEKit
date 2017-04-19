5a3a4323fabfe4b69855119201a78493dde6f3b9
JayZhao <zhaojianyin@icloud.com>
2017-04-19 20:35:29 +0800

    Fix KVO crash.

--- src/RawSocket/NWTCPSocket.swift
+++ src/RawSocket/NWTCPSocket.swift
@@ -226,33 +226,37 @@ public class NWTCPSocket: NSObject, RawTCPSocketProtocol {
     
     override public func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
-        if context == &NWTCPSocket.kKVOConnectionStatusContext && keyPath == "state" {
+        // We have to enclosure it with the same queue where self.deinit gets called.
+        // http://inessential.com/2013/12/20/observers_and_thread_safety
+        QueueFactory.getQueue().async {[weak self] in
             
-            if let connection = self.connection {
-                switch connection.state {
-                case .connected:
-                    queueCall {
-                        self.delegate?.didConnectWith(socket: self)
+            if context == &NWTCPSocket.kKVOConnectionStatusContext && keyPath == "state" {
+                
+                if let strongSelf = self, let connection = strongSelf.connection {
+                    switch connection.state {
+                    case .connected:
+                        strongSelf.delegate?.didConnectWith(socket: strongSelf)
+                    case .disconnected:
+                        strongSelf.cancelled = true
+                        strongSelf.cancel()
+                    case .cancelled:
+                        strongSelf.cancelled = true
+                        
+                        let delegate = strongSelf.delegate
+                        strongSelf.delegate = nil
+                        delegate?.didDisconnectWith(socket: strongSelf)
+                        
+                    default:
+                        break
                     }
-                case .disconnected:
-                    cancelled = true
-                    cancel()
-                case .cancelled:
-                    cancelled = true
-                    queueCall {
-                        let delegate = self.delegate
-                        self.delegate = nil
-                        delegate?.didDisconnectWith(socket: self)
-                    }
-                default:
-                    break
                 }
             }
-            
+            else {
+                // Although not calling super.observeValue violates the standard KVO usage pattern, but `super` can not be used in a closure where self is explicitly captured (Swift3)
+                //super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            }
         }
-        else {
-            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-        }
+        
         
     }
     
