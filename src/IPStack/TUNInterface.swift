4e229f79fee8025a5a6c86d0bde564effde14700
JayZhao <zhaojianyin@icloud.com>
2017-04-20 23:51:51 +0800

    Remove check memory

--- src/IPStack/TUNInterface.swift
+++ src/IPStack/TUNInterface.swift
@@ -59,28 +59,7 @@ open class TUNInterface {
         }
     }
     
-    func checkMemory() -> UInt64 {
-        var taskInfo = mach_task_basic_info()
-        var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size)/4
-        let kerr: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
-            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-                task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
-            }
-        }
-        
-        if kerr == KERN_SUCCESS {
-            DDLogDebug("Memory used in bytes: \(taskInfo.resident_size)")
-            return taskInfo.resident_size
-        }
-        else {
-            DDLogDebug("Error with task_info(): " +
-                (String(cString: mach_error_string(kerr), encoding: String.Encoding.ascii) ?? "unknown error"))
-            return 0
-        }
-    }
-    
     fileprivate func readPackets() {
-        _ = self.checkMemory()
         
         packetFlow?.readPackets { packets, versions in
             QueueFactory.getQueue().async {
